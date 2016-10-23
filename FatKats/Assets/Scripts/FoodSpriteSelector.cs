9b002d65745c858183ab383fbcfee3730efdbcaa
Laura <l4depende@gmail.com>
2016-10-23 12:12:06 +0200

    FoodSpriteSelector

---
+++ FatKats/Assets/Scripts/FoodSpriteSelector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FoodSpriteSelector : MonoBehaviour {
+
+	public Sprite food1;
+	public Sprite food2;
+	public Sprite food3;
+	public Sprite food4;
+	public Sprite food5;
+	public Sprite food6;
+
+	public Sprite giveRandomFood(){
+		switch((int)(Random.Range(1,6))){
+		case 1: return food1;
+			break;
+		case 2: return food2;
+			break;
+		case 3: return food3;
+			break;
+		case 4: return food4;
+			break;
+		case 5: return food5;
+			break;
+		default: return food6;
+			break;
+		}
+	}
+}
