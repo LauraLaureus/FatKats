06746e4f8bdf761aa6c03c64847374457e3918a8
JuanCarlosGdM <juancarlosgdm@gmail.com>
2016-10-23 10:10:56 +0200

    Prefab plataforma y spawner de lana

---
+++ FatKats/Assets/Scripts/WoolSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class WoolSpawner : MonoBehaviour {
+    public GameObject wool;
+    public List<GameObject> woolSpawnPoint;
+    public float minRespawnTime;
+    public float maxRespawnTime;
+
+    private float time;
+    private float nextRespawnTime;
+
+    void Start() {
+        nextRespawnTime = Random.Range(minRespawnTime, maxRespawnTime);
+    }
+
+    void Update() {
+        time += Time.deltaTime;
+        if (time >= nextRespawnTime) {
+            Spawn();
+            time = 0.0f;
+            nextRespawnTime = Random.Range(minRespawnTime, maxRespawnTime);
+        }
+    }
+
+    void Spawn() {
+        GameObject sp = woolSpawnPoint[Random.Range(0, woolSpawnPoint.Count)];
+        Instantiate(wool, sp.transform.position, Quaternion.identity);
+    }
+}
