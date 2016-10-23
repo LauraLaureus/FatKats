dbe84fa7a5aee2504b8e29043b65ca6dcb5b5ffe
Laura <l4depende@gmail.com>
2016-10-23 10:21:46 +0200

    Draw message

---
+++ FatKats/Assets/Scripts/DrawDetector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DrawDetector : MonoBehaviour {
+
+
+	Text currentText;
+
+	// Use this for initialization
+	void Start () {
+		currentText = this.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentText.text.Contains ("Player 1") && currentText.text.Contains ("Player 2"))
+			currentText.text = "DRAW!";
+	}
+}
