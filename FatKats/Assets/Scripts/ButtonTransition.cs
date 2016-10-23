184e66af7d58af1a9c7d3a7c4dce6de582d66eec
Laura <l4depende@gmail.com>
2016-10-23 11:10:10 +0200

    Menu and credits

---
+++ FatKats/Assets/Scripts/ButtonTransition.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;  
+using System.Collections;  
+using UnityEngine.EventSystems;  
+using UnityEngine.UI;
+
+public class ButtonTransition : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	public Text theText;
+
+	public void Start(){
+		theText = this.GetComponentInChildren<Text> ();
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		theText.color = Color.red; //Or however you do your color
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		theText.color = Color.black; //Or however you do your color
+	}
+}
+
