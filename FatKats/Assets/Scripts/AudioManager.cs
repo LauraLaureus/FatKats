3c9515139d5e8d2724e2505654d1ef15ddc831cc
JuanCarlosGdM <juancarlosgdm@gmail.com>
2016-10-26 20:07:51 +0200

    Optimización en tamaño de build y otras modificaciones

--- FatKats/Assets/Scripts/AudioManager.cs
+++ FatKats/Assets/Scripts/AudioManager.cs
@@ -1,21 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour {
-    private static AudioSource normalMiau;
-    private static AudioSource namNam;
+    private static AudioManager instance;
+    private AudioSource button;
+    private AudioSource gameMusic;
+    private AudioSource menuMusic;
+    private AudioSource normalMiau;
+    private AudioSource namNam;
 
-    void Start() {
+    void Awake() {
+        // Patrón Singleton
+        if (instance == null) {
+            instance = this;
+        } else if (!instance.Equals(this)) {
+            Destroy(gameObject);
+        }
+        // Referencias a componentes
         AudioSource[] sources = GetComponents<AudioSource>();
         normalMiau = sources[0];
         namNam = sources[1];
+        menuMusic = sources[2];
+        gameMusic = sources[3];
+        button = sources[4];
+        SceneManager.activeSceneChanged += ChangeSceneTheme;
     }
 
-	public static void PlayNormalMiau() {
-        normalMiau.Play();
+    void ChangeSceneTheme(Scene prev, Scene current) {
+        switch (current.name) {
+            case "Instructions":
+                instance.menuMusic.Stop();
+                break;
+            case "InGame":
+                instance.menuMusic.Stop();
+                if (!instance.gameMusic.isPlaying) {
+                    instance.gameMusic.Play();
+                }
+                break;
+            default:
+                if (!instance.menuMusic.isPlaying) {
+                    instance.menuMusic.Play();
+                }
+                break;
+        }
+    }
+
+    void Start() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static void PlayButton() {
+        instance.button.Play();
+    }
+
+    public static void PlayNormalMiau() {
+        instance.normalMiau.Play();
     }
 
     public static void PlayNamNam() {
-        namNam.Play();
+        instance.namNam.Play();
     }
 }
